8e2fab78a18a323a0b44544635da607c0972cd0b
alisa <alisawalter1@gmail.com>
2019-03-26 18:36:38 +0100

    completed

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,13 +1,46 @@
 package com.londonappbrewery.magiceightball;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.imageViewBall)
+    ImageView imageViewBall;
+    @BindView(R.id.button)
+    Button button;
+
+    private final int[] ballAnswersArray = {
+            R.drawable.ball1,
+            R.drawable.ball2,
+            R.drawable.ball3,
+            R.drawable.ball4,
+            R.drawable.ball5
+    };
+
+    Random random = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                askBall();
+            }
+        });
+    }
+
+    private void askBall() {
+        imageViewBall.setImageResource(ballAnswersArray[random.nextInt(5)]);
     }
 }
